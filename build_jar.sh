cbbb15f84b00aadf91a008065427e1ff1f2786b5
Alexander Grechin <infinum@mail.ru>
2017-07-23 03:46:57 +0300

    add script to start selenium hub with servlet

--- build_jar.sh
+++ build_jar.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 servlet=GridServlet
+build_dir=build
+src_dir=src
 
 mkdir -p build
-wget -q https://selenium-release.storage.googleapis.com/3.4/selenium-server-standalone-3.4.0.jar -O build/selenium-server-standalone.jar
-wget -q http://www.java2s.com/Code/JarDownload/javax.servlet/javax.servlet.jar.zip -O build/javax.servlet.jar.zip
-unzip -o build/javax.servlet.jar.zip -d build/
-wget -q http://www.java2s.com/Code/JarDownload/java-json/java-json.jar.zip -O build/java-json.jar.zip
-unzip -o build/java-json.jar.zip -d build/
+if [ ! -f ${build_dir}/selenium-server-standalone.jar ]; then
+    wget -q https://selenium-release.storage.googleapis.com/3.4/selenium-server-standalone-3.4.0.jar -O ${build_dir}/selenium-server-standalone.jar
+fi
+if [ ! -f ${build_dir}/javax.servlet.jar ]; then
+    wget -q http://www.java2s.com/Code/JarDownload/javax.servlet/javax.servlet.jar.zip -O ${build_dir}/javax.servlet.jar.zip
+    unzip -o ${build_dir}/javax.servlet.jar.zip -d ${build_dir}/
+fi
+if [ ! -f ${build_dir}/java-json.jar ]; then
+    wget -q http://www.java2s.com/Code/JarDownload/java-json/java-json.jar.zip -O ${build_dir}/java-json.jar.zip
+    unzip -o ${build_dir}/java-json.jar.zip -d ${build_dir}/
+fi
+
+#build class (it should be the current page to keep all dependencies)
+cd ${build_dir}
+javac -cp "./*" ../${src_dir}/${servlet}.java -d ./
+jar cvf ./${servlet}.jar ./${servlet}.class
 
 #build class
-javac -cp "build/selenium-server-standalone-.jar:build/*" src/${servlet}.java -d build/
-jar cvf build/${servlet}.jar build/${servlet}.class
+#javac -cp "${build_dir}/*" ${src_dir}/${servlet}.java -d ${build_dir}/
+#jar cvf ${servlet}.jar ${build_dir}/${servlet}.class
+
---
+++ start_with_servlet.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+servlet=GridServlet
+build_dir=build
+
+java -cp "${build_dir}/*" org.openqa.grid.selenium.GridLauncherV3 -role hub -port 4444 -servlets ${servlet}
